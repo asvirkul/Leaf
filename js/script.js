f058f1e2525e2a21f6baa5c6bc2e4e9188c9ca04
Anton <asvirkul@gmail.com>
2021-10-20 12:45:47 +0300

    about page

--- js/script.js
+++ js/script.js
@@ -1,96 +1,16 @@
 jQuery(document).ready(function($) {
 
-    var myVideo = document.getElementById("video-main"); 
-    $('.video-wrapper .play-vid').hide();
-    $('.video-control').click(function () {
-    if ( $('.video-wrapper .play-vid').is(':hidden') ) {
-        $('.video-wrapper .pause-vid').hide();
-        $('.video-wrapper .play-vid').show();
-    }
-    else {
-        $('.video-wrapper .play-vid').hide();
-        $('.video-wrapper .pause-vid').show();
-    }
-        if (myVideo.paused) 
-            myVideo.play(); 
-        else 
-            myVideo.pause(); 
-    } 
-) 
-    var sale = $('.sale-anim');
-    sale.hover(function () {
-        $(this).animate({
-            top: '-10px'
-        }, 10)
-    }, function() {
-        $(this).animate({
-            top: '0px'
-        }, 10);
-    });
-    // Video End
 
-    /* Slider Start */
-    const swiper = new Swiper('.swiper-main', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true, 
-        autoplay: true,
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
 
-        effect: 'fade',
-        fadeEffect: {
-            crossFade: true
-        }
-      })
-      const swiperAbout = new Swiper('.swiper', {
-        slidesPerView: "auto",
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true, 
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        }
-      })
-
-      /* Slider End */
-
-      $('.materialboxed').materialbox();
-
-      var hHeader = $('header').height();
+    /* Animation Header Start */
+    var hHeader = $('header').height();
     $(window).scroll(function () {
         var scroll = getCurrentScroll();
         if (scroll >= hHeader) {
             $('header').addClass('bg-scroll');
             $('header').css({
-                'background': '#fff'
+                'background': '#fff',
+                'border-bottom': '1px solid #DEDEDE'
             })
             $('#logo-pic').attr('src', 'images/razor.png')
             $('header .menu li a').css({
@@ -130,10 +50,92 @@ jQuery(document).ready(function($) {
     function getCurrentScroll() {
         return window.pageYOffset || document.documentElement.scrollTop;
     } 
-    //header
+    /* Animation Header End */
+
+
+
+
+    /* Slick Start */
+    $('.slick-wrapper').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+            }
+        ]
+      });
+      $('.eco-wrapper').slick({
+        dots: true,
+        arrows: false,
+        dots: true,
+        fade: true,
+        speed: 400,
+        infinite: true,
+        cssEase: 'ease-in-out'
+      });
+    /* Slick End */
+
+    /* Video Start */
+    var myVideo = document.getElementById("video-main"); 
+    $('.video-wrapper .play-vid').hide();
+    $('.video-control').click(function () {
+    if ( $('.video-wrapper .play-vid').is(':hidden') ) {
+        $('.video-wrapper .pause-vid').hide();
+        $('.video-wrapper .play-vid').show();
+    }
+    else {
+        $('.video-wrapper .play-vid').hide();
+        $('.video-wrapper .pause-vid').show();
+    }
+        if (myVideo.paused) 
+            myVideo.play(); 
+        else 
+            myVideo.pause(); 
+    } 
+    ) 
+    /* Video End */
+
+    /* Animation Sale Start */
+    var sale = $('.sale-anim');
+    sale.hover(function () {
+        $(this).animate({
+            top: '-10px'
+        }, 10)
+    }, function() {
+        $(this).animate({
+            top: '0px'
+        }, 10);
+    });
+    /* Animation Sale End */
+
+    $('.materialboxed').materialbox();
+
+    
 
     $(document).ready(function(){
         $('.sidenav').sidenav();
       });
+      // Sidenav End
 
 }); //ready
---
+++ js/swiper.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function($) {
+    /* Slider Start */
+    const swiper = new Swiper('.swiper-main', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true, 
+        autoplay: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        }
+      });
+      /* Slider End */
+});
