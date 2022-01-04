a90556902baf625fcdd30641a74a902810f71675
Anton <asvirkul@gmail.com>
2022-01-04 17:30:54 +0300

    Animation Instruction Start

--- js/script.js
+++ js/script.js
@@ -1,4 +1,97 @@
 jQuery(document).ready(function($) {
+    /* Instruction Animation Start */
+    var heightBlocks = $('#tabs, .tabs-img, .instructions-title');
+    var heightInstruct = heightBlocks.height();
+    var leftWidth = $('#left-scroll img').width();
+    var rightWidth = $('#right-scroll img').width();
+    $('#left-scroll img').css("left", -leftWidth);
+    $('#right-scroll img').css("right", -rightWidth);
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 0) {
+        $('#left-scroll img').animate({'left':'0px'},500);
+        $('#right-scroll img').animate({'right':'0px'},500);  
+        }
+        else {
+            $('#left-scroll img').css("left", -leftWidth);
+            $('#right-scroll img').css("right", -rightWidth);
+        } 
+    }); 
+    /* Instruction Animation End */
+        $('.sidenav').sidenav();
+        $('#closenav').click(function(){
+            $('.sidenav').sidenav('close');
+          });
+    $('.materialboxed').materialbox();
+    /* Video Start */
+        var myVideo = document.getElementById("video-main"); 
+        $('.video-wrapper .play-vid').hide();
+        $('.video-control').click(function () {
+        if ( $('.video-wrapper .play-vid').is(':hidden') ) {
+            $('.video-wrapper .pause-vid').hide();
+            $('.video-wrapper .play-vid').show();
+        }
+        else {
+            $('.video-wrapper .play-vid').hide();
+            $('.video-wrapper .pause-vid').show();
+        }
+            if (myVideo.paused) 
+                myVideo.play(); 
+            else 
+                myVideo.pause(); 
+        } 
+        ) 
+    /* Video End */
+    /* Animation Header Start */
+    var hHeader = $('.main').height();
+    $(window).scroll(function () {
+        var scroll = getCurrentScroll();
+        if (scroll >= hHeader) {
+            $('.main').addClass('bg-scroll');
+            $('.main').css({
+                'background': '#fff',
+                'border-bottom': '1px solid #DEDEDE'
+            })
+            $('.main #logo-pic').attr('src', 'images/razor.png')
+            $('.main .menu li a').css({
+                'color': '#797979'
+            })
+            $('.sidenav-trigger').css({
+                'color': '#797979'
+            })
+            $('.main i').css({
+                'color': '#000',
+            })
+            $('.main .line-logo').css({
+                'background': '#000'
+            })
+        }
+        else {
+            $('.main').removeClass('bg-scroll');
+            $('.main #logo-pic').attr('src', 'images/razor-white.png')
+            $('.main .menu li a').css({
+                'color': '#fff'
+            })
+            $('.sidenav-trigger').css({
+                'color': '#fff'
+            })
+            $('.main').css({
+                'background': 'transparent',
+                'border': 'none'
+            })
+            $('.main i').css({
+                'color': 'white'
+            })
+            $('.main .line-logo').css({
+                'background': '#fff',
+                'color': 'white'
+            })
+        }
+    });
+    function getCurrentScroll() {
+        return window.pageYOffset || document.documentElement.scrollTop;
+    } 
+    /* Animation Header End */
+    /* Slick SLider Start */
     $('.slick-wrapper').slick({
         centerMode: true,
         centerPadding: '60px',
@@ -71,80 +164,7 @@ jQuery(document).ready(function($) {
         focusOnSelect: true // указываем что бы слайделось по клику
     });
     /* Slick Instruction End */
-        $('.sidenav').sidenav();
-        $('#closenav').click(function(){
-            $('.sidenav').sidenav('close');
-          });
-    $('.materialboxed').materialbox();
-    var myVideo = document.getElementById("video-main"); 
-    $('.video-wrapper .play-vid').hide();
-    $('.video-control').click(function () {
-    if ( $('.video-wrapper .play-vid').is(':hidden') ) {
-        $('.video-wrapper .pause-vid').hide();
-        $('.video-wrapper .play-vid').show();
-    }
-    else {
-        $('.video-wrapper .play-vid').hide();
-        $('.video-wrapper .pause-vid').show();
-    }
-        if (myVideo.paused) 
-            myVideo.play(); 
-        else 
-            myVideo.pause(); 
-    } 
-    ) 
 
-    /* Animation Header Start */
-    var hHeader = $('.main').height();
-    $(window).scroll(function () {
-        var scroll = getCurrentScroll();
-        if (scroll >= hHeader) {
-            $('.main').addClass('bg-scroll');
-            $('.main').css({
-                'background': '#fff',
-                'border-bottom': '1px solid #DEDEDE'
-            })
-            $('.main #logo-pic').attr('src', 'images/razor.png')
-            $('.main .menu li a').css({
-                'color': '#797979'
-            })
-            $('.sidenav-trigger').css({
-                'color': '#797979'
-            })
-            $('.main i').css({
-                'color': '#000',
-            })
-            $('.main .line-logo').css({
-                'background': '#000'
-            })
-        }
-        else {
-            $('.main').removeClass('bg-scroll');
-            $('.main #logo-pic').attr('src', 'images/razor-white.png')
-            $('.main .menu li a').css({
-                'color': '#fff'
-            })
-            $('.sidenav-trigger').css({
-                'color': '#fff'
-            })
-            $('.main').css({
-                'background': 'transparent',
-                'border': 'none'
-            })
-            $('.main i').css({
-                'color': 'white'
-            })
-            $('.main .line-logo').css({
-                'background': '#fff',
-                'color': 'white'
-            })
-        }
-    });
-    function getCurrentScroll() {
-        return window.pageYOffset || document.documentElement.scrollTop;
-    } 
-    /* Animation Header End */
-    
     /* Tabs */
     $('#tabs li a:not(:first)').addClass('inactive');
     $('.hide-tabs').hide();
