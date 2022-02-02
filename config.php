129974ecd40e0e7e1ded7a2d98d95f0a0706840d
Anton <asvirkul@gmail.com>
2022-02-02 19:55:37 +0200

    Engine Start

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('CONTROLLER', 'controller/controller.php');
+define('MODEL', 'model/model.php');
+define('VIEW', 'view/');
+define('PATH', 'htpp://leaf.loc');
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DB', 'leaf');
